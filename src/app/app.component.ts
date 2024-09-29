1904e379c0757458d35abe66bd7b95419fb9af25
Leo Loureiro <leolou.90@gmail.com>
2024-09-28 21:27:33 -0700

    add components to imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../components/header/header.component";
 import { FooterComponent } from "../components/footer/footer.component";
 import { MainSectionComponent } from "../components/main-section/main-section.component";
+import { DesignSectionComponent } from "../components/design-section/design-section.component";
+import { TeamSectionComponent } from "../components/team-section/team-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainSectionComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainSectionComponent, DesignSectionComponent, TeamSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
