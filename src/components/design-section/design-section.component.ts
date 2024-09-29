26dba8362a61e949088d3eb2d6b5d82ef92294f4
Leo Loureiro <leolou.90@gmail.com>
2024-09-28 21:30:27 -0700

    add design section semantic and styles on desktop, tablet and mobile

---
+++ src/components/design-section/design-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'design-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './design-section.component.html',
+  styleUrl: './design-section.component.scss'
+})
+export class DesignSectionComponent {
+
+}
