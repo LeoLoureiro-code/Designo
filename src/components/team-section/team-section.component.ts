801f017c11fd9556d96caab82baf8158ebe2bc3e
Leo Loureiro <leolou.90@gmail.com>
2024-09-28 21:29:43 -0700

    add team section semantic and styles on desktop, tablet and mobile

---
+++ src/components/team-section/team-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'team-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './team-section.component.html',
+  styleUrl: './team-section.component.scss'
+})
+export class TeamSectionComponent {
+
+}
