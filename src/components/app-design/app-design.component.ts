003bb131c17b52eb832b762c027f4c9208a0b5a2
Leo Loureiro <leolou.90@gmail.com>
2024-09-29 19:12:12 -0700

    add about us, app design, contact, graphic design, locations, and web design components

---
+++ src/components/app-design/app-design.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-design',
+  standalone: true,
+  imports: [],
+  templateUrl: './app-design.component.html',
+  styleUrl: './app-design.component.scss'
+})
+export class AppDesignComponent {
+
+}
