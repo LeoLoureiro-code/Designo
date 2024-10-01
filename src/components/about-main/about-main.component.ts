e80de3cfb9587e4632a8d19f5dce01d7fcc9b7f9
Leo Loureiro <leolou.90@gmail.com>
2024-09-30 19:32:16 -0700

    create about main component

---
+++ src/components/about-main/about-main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-main.component.html',
+  styleUrl: './about-main.component.scss'
+})
+export class AboutMainComponent {
+
+}
