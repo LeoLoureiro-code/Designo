f8127ac13105a814ca8f39d5327559f8d4e875d1
Leo Loureiro <leolou.90@gmail.com>
2024-09-30 19:30:57 -0700

    add the child  components to home page component

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { AboutMainComponent } from "../about-main/about-main.component";
+import { MainSectionComponent } from '../main-section/main-section.component';
+import { DesignSectionComponent } from '../design-section/design-section.component';
+import { TeamSectionComponent } from '../team-section/team-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [AboutMainComponent, MainSectionComponent, DesignSectionComponent, TeamSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
