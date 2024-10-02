969bda7a0407ae23a1584761bbd96842d54f20a4
Leo Loureiro <leolou.90@gmail.com>
2024-10-01 19:23:55 -0700

    add routerLink to imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from '../components/home/home.component';
 import { AboutUsComponent } from '../components/about-us/about-us.component';
 import { ContactUsComponent } from '../components/contact-us/contact-us.component';
+import { LocationsComponent } from '../components/locations/locations.component';
 
 export const routes: Routes = [
     //Add the home when there is no parameters or make it start in home
     {path: 'home', component: HomeComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'about', component: AboutUsComponent},
-    {path: 'locations', component: AboutUsComponent},
+    {path: 'locations', component: LocationsComponent},
     {path: 'contact', component: ContactUsComponent},
 
 ];
