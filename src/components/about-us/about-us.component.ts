5baa25f6cd375fbcd66087fd7d0cf6846ab2fe2e
Leo Loureiro <leolou.90@gmail.com>
2024-10-02 19:23:32 -0700

    add company history component

--- src/components/about-us/about-us.component.ts
+++ src/components/about-us/about-us.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CompanyHistoryComponent } from "../company-history/company-history.component";
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [],
+  imports: [CompanyHistoryComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
