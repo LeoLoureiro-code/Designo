7cc450c3fdd40cae978c6866889f378674a43e3e
Leo Loureiro <leolou.90@gmail.com>
2024-10-02 19:24:21 -0700

    create company history and add desktop styles in title and pararaph

---
+++ src/components/company-history/company-history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'company-history',
+  standalone: true,
+  imports: [],
+  templateUrl: './company-history.component.html',
+  styleUrl: './company-history.component.scss'
+})
+export class CompanyHistoryComponent {
+
+}
