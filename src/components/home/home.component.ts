2d04576cee84a0e34d884a186d464e01ee7f338b
Leo Loureiro <leolou.90@gmail.com>
2024-10-05 22:13:21 -0700

    add routerModuel to home

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AboutMainComponent } from "../about-main/about-main.component";
+
 import { MainSectionComponent } from '../main-section/main-section.component';
 import { DesignSectionComponent } from '../design-section/design-section.component';
 import { TeamSectionComponent } from '../team-section/team-section.component';
@@ -7,7 +7,7 @@ import { TeamSectionComponent } from '../team-section/team-section.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AboutMainComponent, MainSectionComponent, DesignSectionComponent, TeamSectionComponent],
+  imports: [ MainSectionComponent, DesignSectionComponent, TeamSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
