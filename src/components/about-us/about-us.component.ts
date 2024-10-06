4735fdd5b24cc865f9ac0ce22836d21841e33b44
Leo Loureiro <leolou.90@gmail.com>
2024-10-05 22:12:46 -0700

    add about us section with media queries

--- src/components/about-us/about-us.component.ts
+++ src/components/about-us/about-us.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { CompanyHistoryComponent } from "../company-history/company-history.component";
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [CompanyHistoryComponent],
+  imports: [],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
