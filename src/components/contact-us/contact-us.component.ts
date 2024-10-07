155ebec086c018e004e9fe91609d45017f3b42b2
Leo Loureiro <leolou.90@gmail.com>
2024-10-06 19:18:10 -0700

    add contact html semantic

--- src/components/contact-us/contact-us.component.ts
+++ src/components/contact-us/contact-us.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LocationsNavComponent } from "../locations-nav/locations-nav.component";
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [],
+  imports: [LocationsNavComponent],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
