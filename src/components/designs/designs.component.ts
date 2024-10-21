99a310c593f2f8b02e445d28a6290a2a77154011
Leo Loureiro <leolou.90@gmail.com>
2024-10-20 19:36:25 -0700

    add designs template

---
+++ src/components/designs/designs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-designs',
+  standalone: true,
+  imports: [],
+  templateUrl: './designs.component.html',
+  styleUrl: './designs.component.scss'
+})
+export class DesignsComponent {
+
+}
