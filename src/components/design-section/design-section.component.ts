6855461286b827f3465c03db025a43a0f9453177
Leo Loureiro <leolou.90@gmail.com>
2024-10-21 19:16:03 -0700

    add routerlink to anchors

--- src/components/design-section/design-section.component.ts
+++ src/components/design-section/design-section.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'design-section',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './design-section.component.html',
   styleUrl: './design-section.component.scss'
 })
