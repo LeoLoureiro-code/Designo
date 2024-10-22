0dfe1d76ef1cac79410d10376a0b413965ab4030
Leo Loureiro <leolou.90@gmail.com>
2024-10-21 19:18:01 -0700

    add new route to Designs components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,12 @@ import { TeamSectionComponent } from "../components/team-section/team-section.co
 import { AboutUsComponent } from '../components/about-us/about-us.component';
 import { LocationsComponent } from '../components/locations/locations.component';
 import { ContactUsComponent } from '../components/contact-us/contact-us.component';
+import { DesignsComponent } from '../components/designs/designs.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainSectionComponent, DesignSectionComponent, TeamSectionComponent, AboutUsComponent, LocationsComponent,ContactUsComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainSectionComponent, DesignSectionComponent, TeamSectionComponent, AboutUsComponent, LocationsComponent,ContactUsComponent, DesignsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from '../components/home/home.component';
 import { AboutUsComponent } from '../components/about-us/about-us.component';
 import { ContactUsComponent } from '../components/contact-us/contact-us.component';
 import { LocationsComponent } from '../components/locations/locations.component';
+import { DesignsComponent } from '../components/designs/designs.component';
 
 export const routes: Routes = [
     //Add the home when there is no parameters or make it start in home
@@ -11,5 +12,6 @@ export const routes: Routes = [
     {path: 'about', component: AboutUsComponent},
     {path: 'locations', component: LocationsComponent},
     {path: 'contact', component: ContactUsComponent},
+    {path: 'designs', component: DesignsComponent},
 
 ];
