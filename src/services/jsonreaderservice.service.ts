29c4af1dab858c30992ddc22645927693c1fc793
Leo Loureiro <leolou.90@gmail.com>
2024-10-28 19:29:46 -0700

    fix httpClient

--- src/services/jsonreaderservice.service.ts
+++ src/services/jsonreaderservice.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class JSONReaderserviceService {
+export class JSONReaderService {
+
+  private jsonUrl = 'assets/design.json';
 
   constructor(private httpClient: HttpClient) 
-  { 
-    
+  {   }
 
+  getJsonData(): Observable<any> {
+    return this.httpClient.get<any>(this.jsonUrl);
   }
+
 }
