463c50f5abddf1165be2cd61c7612adb11200fd0
Leo Loureiro <leolou.90@gmail.com>
2024-11-30 23:34:33 -0700

    add validations to contact form

--- src/components/contact-us/contact-us.component.ts
+++ src/components/contact-us/contact-us.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
-import { LocationsNavComponent } from "../locations-nav/locations-nav.component";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LocationsNavComponent } from '../locations-nav/locations-nav.component';
+import { ReactiveFormsModule } from '@angular/forms'; 
+import { CommonModule } from '@angular/common';  
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [LocationsNavComponent],
+  imports: [LocationsNavComponent, ReactiveFormsModule, CommonModule],  
   templateUrl: './contact-us.component.html',
-  styleUrl: './contact-us.component.scss'
+  styleUrls: ['./contact-us.component.scss']
 })
-export class ContactUsComponent {
+export class ContactUsComponent implements OnInit {
+  contactForm!: FormGroup;  
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      console.log('Form submitted:', this.contactForm.value);
+
+     
+      this.contactForm.reset();
+
+      this.contactForm.markAsPristine();
+      this.contactForm.markAsUntouched();
+    } else {
+      console.error('Form is invalid');
+    }
+  }
 }
